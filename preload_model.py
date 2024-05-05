d475df9142d772ef121fe1722c3a4df158784d54
Alarez777 <60858814+Alarez777@users.noreply.github.com>
2024-05-05 10:55:43 -0600

    Actualizar preload_model.py

--- preload_model.py
+++ preload_model.py
@@ -1,6 +1,16 @@
 import whisperx
 
-model = whisperx.load_model("large-v2", "cpu", compute_type="float32")
+#model = whisperx.load_model("large-v2", "cpu", compute_type="float32")
 model = whisperx.load_model("large-v2", "cpu", compute_type="float32", language="es")
 audio = whisperx.load_audio("./test.aac")
 transcription = model.transcribe(audio, batch_size=5)
+model_a, metadata = whisperx.load_align_model(
+            language_code=self.transcription["language"], device=self.device
+transcription = whisperx.align(
+            transcription["segments"],
+            model_a,
+            metadata,
+            audio,
+            cpu,
+            return_char_alignments=False,
+        )
