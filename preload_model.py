ce99e9b1d985093236be1e7c72789523d9151b0c
Alarez777 <60858814+Alarez777@users.noreply.github.com>
2024-05-05 10:51:26 -0600

    Actualizar preload_model.py

--- preload_model.py
+++ preload_model.py
@@ -3,3 +3,4 @@ import whisperx
 model = whisperx.load_model("large-v2", "cpu", compute_type="float32")
 model = whisperx.load_model("large-v2", "cpu", compute_type="float32", language="es")
 audio = whisperx.load_audio("./test.aac")
+transcription = model.transcribe(audio, batch_size=5)
